0354369afe429fc964788746bfbdf61cdd618010
cwc <43362456+ruoshy@users.noreply.github.com>
2019-06-04 11:10:45 +0800

    Update BookResource.java

--- src/main/java/cn/ousky/mo/resource/BookResource.java
+++ src/main/java/cn/ousky/mo/resource/BookResource.java
@@ -12,10 +12,12 @@ public class BookResource {
     private String resourcePath;
 
     public String getCatalog(int id) {
+        // 获取本地book目录内容
         return new ReadFile().getContent(resourcePath + "/" + id + "/0.txt");
     }
 
     public Content getChapter(int id, int page) {
+        // 获取本地book章节内容
         Content content = new Content();
         content.setContent(new ReadFile().getContent(resourcePath + "/" + id + "/" + page + ".txt"));
         return content;
